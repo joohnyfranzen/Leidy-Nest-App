5d4bd884a5c9148fd19992102ec7b1ca365f8e28
Your Name <you@example.com>
2022-08-19 19:36:14 -0300

    Rota Login e register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { DtoAuth } from "./auth_dto";
 import { AuthDto } from "./dto";
 
 @Controller('auth')
@@ -8,14 +9,25 @@ export class AuthController {
 
     @HttpCode(HttpStatus.CREATED)
     @Post('signup')
-    signin(@Body() dto: AuthDto) {
+    signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto);
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('signin')
-    signup(@Body() dto: AuthDto) {
+    signin(@Body() dto: AuthDto) {
         return this.authService.signin(dto);
     }
 
+    @HttpCode(HttpStatus.CREATED)
+    @Post('register')
+    register(@Body() dto: DtoAuth) {
+        return this.authService.register(dto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() dto: DtoAuth) {
+        return this.authService.login(dto);
+    }
 }
