d31a4a757c623465a07ade25ff23260c1904c4fd
Your Name <you@example.com>
2022-08-22 11:59:03 -0300

    Order Post create function

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { OrderDto } from './dto/order.dto';
+import { OrderService } from './order.service';
 
+@UseGuards(JwtGuard)
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private orderService: OrderService) {}
+
+    @HttpCode(HttpStatus.CREATED)
+    @Post('create')
+    newOrder(
+        @Body() dto: OrderDto, 
+        @GetUser('id') userId: number
+        ) {
+        return this.orderService.newOrder(userId, dto);
+    }
+
+    @HttpCode(HttpStatus.ACCEPTED)
+    @Get('show')
+    showOrders() 
+    {
+        return this.orderService.showOrder();
+    }
+}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { OrderDto } from './dto/order.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor ( 
+        private prisma: PrismaService,
+        ) {}
+
+    // ORDER Functions
+
+    // New Order
+
+    async newOrder(userId: number, dto: OrderDto, ) {
+        const newOrder = await this.prisma.order.create({
+            data: {
+                title: dto.title, 
+                type: dto.type, 
+                description: dto.description, 
+                price: dto.price, 
+                month_date_start: dto.month_date_start, 
+                month_date_end: dto.month_date_end, 
+                weekly: dto.weekly, 
+                week_day_start: dto.week_day_start, 
+                week_day_end: dto. 
+                week_day_end, 
+                daily: dto.daily, 
+                weekend: dto.weekend, 
+                holiday: dto.holiday,
+                userId,
+            }})
+        return newOrder
+    }
+
+    // Show Orders
+
+    async showOrders()
+}
