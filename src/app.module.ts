0f1f383ac5e3fa66c948270886d58f793e831692
Your Name <you@example.com>
2022-08-22 07:57:29 -0300

    Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,14 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthEmployeeModule } from './auth/employee/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { DashboardController } from './dashboard/dashboard.controller';
-import { DashboardService } from './dashboard/dashboard.service';
 import { RequestModule } from './request/request.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }), AuthModule, UserModule, PrismaModule, AuthEmployeeModule, DashboardModule, RequestModule],
-  controllers: [DashboardController],
-  providers: [DashboardService],
+  controllers: [],
+  providers: [],
 
 })
 export class AppModule {}
