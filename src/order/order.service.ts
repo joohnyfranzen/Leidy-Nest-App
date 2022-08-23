7adebdf4623da037e46d22c1e0efdd9a22f5b528
Your Name <you@example.com>
2022-08-23 09:22:00 -0300

    Dto update

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, UseGuards } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import select from 'src/utils/select';
 import { OrderDto } from './dto/order.dto';
 
 @Injectable()
@@ -13,27 +14,47 @@ export class OrderService {
     // New Order
 
     async newOrder(userId: number, dto: OrderDto, ) {
+
+        dto.price = +dto.price;
+
         const newOrder = await this.prisma.order.create({
-            data: {
-                title: dto.title, 
-                type: dto.type, 
-                description: dto.description, 
-                price: dto.price, 
-                month_date_start: dto.month_date_start, 
-                month_date_end: dto.month_date_end, 
-                weekly: dto.weekly, 
-                week_day_start: dto.week_day_start, 
-                week_day_end: dto. 
-                week_day_end, 
-                daily: dto.daily, 
-                weekend: dto.weekend, 
-                holiday: dto.holiday,
-                userId,
-            }})
+            data: Object.assign( dto, {userId} )
+            })
         return newOrder
     }
 
     // Show Orders
 
-    async showOrders()
+    async show(id : number) {
+        try {
+            const order = await this.prisma.order.findFirst({where: {id: +id}})
+            return order
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    // Search
+
+    async search(query){
+        const { q, order, orderAsc = 'asc' } = query
+
+        try{
+            const querySearch = await this.prisma.order.findMany({
+                select: select(['id', 'createdAt', 'title', 'type', 'description']),
+                where: {
+                    OR:[
+                        { title: {contains: q} },
+                        { description: {contains: q} },
+                        { type: {contains: q} },
+                        +q ? { price: {equals: +q} } : null
+                    ]
+                },
+                    orderBy: order ? {[order]: orderAsc} : { createdAt: 'desc' }
+            })
+            return querySearch;
+        }catch(error){
+            console.log(error)
+        }
+    }
 }
