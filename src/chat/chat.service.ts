1abdb51da234408e18eddbc8cf2e4bf88381dac4
Your Name <you@example.com>
2022-08-23 21:24:18 -0300

    Chat Service, Controller

---
+++ src/chat/chat.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatDto } from './dto';
+
+@Injectable()
+export class ChatService {
+    constructor (
+        private prisma: PrismaService,
+        ) {}
+
+        // Chat Functions
+
+        // New Chat
+
+        async createMessage(userId: number, dto: ChatDto) {
+
+            const newMessage = await this.prisma.chat.create({
+                data: Object.assign(dto, 
+                    {
+                        userFromId: userId, 
+                        userToId: userId,
+                    }
+                )
+            })
+            return newMessage;
+        }
+}
