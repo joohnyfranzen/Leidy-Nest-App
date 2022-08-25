6ffd29054f244a950b119ee2ec34a29804ce8119
Your Name <you@example.com>
2022-08-25 09:06:25 -0300

    Rating import

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { AuthEmployeeModule } from './auth/employee/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { OrderModule } from './order/order.module';
 import { ChatModule } from './chat/chat.module';
+import { RatingModule } from './rating/rating.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), AuthModule, UserModule, PrismaModule, AuthEmployeeModule, DashboardModule, OrderModule, ChatModule],
+  }), AuthModule, UserModule, PrismaModule, AuthEmployeeModule, DashboardModule, OrderModule, ChatModule, RatingModule],
   controllers: [],
   providers: [],
 
